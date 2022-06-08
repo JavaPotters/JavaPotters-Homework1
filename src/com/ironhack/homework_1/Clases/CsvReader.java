5757742aeb5bf95fa0483f51a342fa1e5c43d330
Sofía de la Fuente <ec.sofiadelafuente@gmail.com>
2022-06-08 23:18:58 +0200

    Class CsvReader has been created

---
+++ src/com/ironhack/homework_1/Clases/CsvReader.java
@@ -0,0 +1,53 @@
+package com.ironhack.homework_1.Clases;
+
+import com.ironhack.homework_1.Clases.Character.Warrior;
+import com.ironhack.homework_1.Clases.Character.Wizard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class CsvReader {
+
+    public void readCSV() {
+        ArrayList<ArrayList<String>> characterList = new ArrayList<ArrayList<String>>();
+        StringBuilder sb = new StringBuilder();
+        Path filePath = Paths.get("/Users/sofiadelafuente/Desktop/java-bootcamp/unit1/homework-1/src/com/ironhack/homework_1/characters.csv");
+        try (BufferedReader br = Files.newBufferedReader(filePath)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] lineData = line.split(",");
+                /*
+
+                if (lineData[0] == "Wizard") {
+                    Warrior warrior = new Warrior(Integer.parseInt(lineData[1]), lineData[2],
+                            Integer.parseInt(lineData[3]), Boolean.parseBoolean(lineData[4]),
+                            Double.parseDouble(lineData[5]), Double.parseDouble(lineData[6]));
+                    System.out.println(warrior + "\n");
+                } else {
+                    Wizard wizard = new Wizard(Integer.parseInt(lineData[1]), lineData[2],
+                            Integer.parseInt(lineData[3]), Boolean.parseBoolean(lineData[4]),
+                            Double.parseDouble(lineData[5]), Double.parseDouble(lineData[6]));
+                    System.out.println(wizard + "\n");
+                }
+
+                 */
+                ArrayList<String> temporalData = new ArrayList<String>();
+                for (String data : lineData) {
+                    temporalData.add(data);
+                }
+                characterList.add(temporalData);
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sb.toString());
+    }
+}
+
--- src/com/ironhack/homework_1/main/Main.java
+++ src/com/ironhack/homework_1/main/Main.java
@@ -3,6 +3,7 @@ package com.ironhack.homework_1.main;
 import com.ironhack.homework_1.Clases.Character.Character;
 import com.ironhack.homework_1.Clases.Character.Warrior;
 import com.ironhack.homework_1.Clases.Character.Wizard;
+import com.ironhack.homework_1.Clases.CsvReader;
 import com.ironhack.homework_1.Clases.Party.CustomizedParty;
 import com.ironhack.homework_1.Clases.Party.Party;
 import com.ironhack.homework_1.Clases.Party.RandomParty;
@@ -19,40 +20,13 @@ import static com.ironhack.homework_1.Clases.Properties.CustomizedProperties.*;
 
 public class Main {
 
-    /*
-    // To read the CSV File we'll need to create:
-    public static final String SEPARATOR=";";
-    public static final String QUOTE="\"";
-
-     */
-
     public static void main(String[] args) throws IOException {
+/*
 
-        // Code to read the CSV File
+        CsvReader lectorCSV = new CsvReader();
+        lectorCSV.readCSV();
 
-        /*
-        BufferedReader br = null;
-
-        try {
-
-            br =new BufferedReader(new FileReader("unit1/characters.csv"));
-            String line = br.readLine();
-            while (null!=line) {
-                String [] fields = line.split(SEPARATOR);
-                System.out.println(Arrays.toString(fields));
-
-                line = br.readLine();
-            }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (null!=br) {
-                br.close();
-            }
-        }
-
-         */
+ */
 
         // Aquí comienza el juego:
         System.out.println("Bienevenido al ... de JavaPotters");
@@ -60,9 +34,9 @@ public class Main {
 
         int option;
         do{
-            System.out.println("Por favor seleccionar: \n" +
-                    "1. Si quieres una full party random \n" +
-                    "2. Si quieres crear tus propios personajes \n");
+            System.out.println("Please, select one option: \n" +
+                    "1. Choose 1 to create a full random party \n" +
+                    "2. Choose 2 to create a customized party \n");
             option = scanner.nextInt();
         } while (option != 1 && option != 2);
 
@@ -73,26 +47,26 @@ public class Main {
             Party team2;
             switch(option){
                 case 1:
-                    System.out.println("Has elegido crear una full party random");
-                    System.out.println("Cuantos jugadores entre 1-5 tiene cada equipo?");
+                    System.out.println("You have chosen to create a full random party");
+                    System.out.println("How many players between 1-5 does each team have?");
 
                     numMemberParty = scanner.nextInt();
                     while (numMemberParty < 0 || numMemberParty > 5){
-                        System.out.println("Ese numero no es valido \n Cuantos jugadores entre 1-5 tiene cada equipo?");
+                        System.out.println("This number isn't valid \n How many players between 1-5 does each team have?");
                         numMemberParty = scanner.nextInt();
                     }
 
                     // Team 1 -------------------------------------------
-                    System.out.println("¿Cual quieres que sea el nombre del equipo 1?");
+                    System.out.println("What do you want your team 1 to be called?");
                     nameTeam1 = scanner.next();
-                    System.out.println("El nombre del Team 1 es: " + nameTeam1);
+                    System.out.println("The name of the team 1 is: " + nameTeam1);
                     team1 = new RandomParty(numMemberParty, nameTeam1);
                     team1.printParty();
 
                     // Team 2 -------------------------------------------
-                    System.out.println("¿Cual quieres que sea el nombre del equipo 2?");
+                    System.out.println(("What do you want your team 2 to be called?"));
                     nameTeam2 = scanner.next();
-                    System.out.println("El nombre del Team 2 es: " + nameTeam2);
+                    System.out.println("The name of the team 2 is: " + nameTeam2);
                     team2 = new RandomParty(numMemberParty, nameTeam2);
                     team2.printParty();
 
@@ -100,88 +74,88 @@ public class Main {
                     break;
 
                 case 2:
-                    System.out.println("Has elegido crear tus propios personajes!");
-                    System.out.println("Cuantos jugadores entre 1-5 tiene cada equipo?");
+                    System.out.println("You have chosen to create your own custom party");
+                    System.out.println("How many players between 1-5 does each team have?");
                     numMemberParty = scanner.nextInt();
                     while (numMemberParty < 0 || numMemberParty > 5){
-                        System.out.println("Ese numero no es valido \n Cuantos jugadores entre 1-5 tiene cada equipo?");
+                        System.out.println("This number isn't valid \n How many players between 1-5 does each team have?");
                         numMemberParty = scanner.nextInt();
                     }
 
                     // Team 1 -------------------------------------------
-                    System.out.println("¿Cual quieres que sea el nombre del equipo 1?");
+                    System.out.println("What do you want your team 1 to be called?");
                     nameTeam1 = scanner.next();
-                    System.out.println("El nombre del Team 1 es: " + nameTeam1);
+                    System.out.println("The name of the team 1 is: " + nameTeam1);
                     team1 = new CustomizedParty(numMemberParty, nameTeam1);
 
                     for(int i = 1; i <= numMemberParty; i++){
                         Scanner scanner2 = new Scanner(System.in);
-                        System.out.println("Por favor seleccionar: \n" +
-                                "1. Si quieres crear un Wizard \n" +
-                                "2. Si quieres crear un Warrior \n");
+                        System.out.println("Please select: \n" +
+                                "1. If you want to create a wizard \n" +
+                                "2. If you want to create a warrior \n");
                         option = scanner2.nextInt();
                         while (option != 1 && option != 2){
-                            System.out.println("Numero invalido");
-                            System.out.println("Por favor seleccionar: \n" +
-                                    "1. Si quieres crear un Wizard \n" +
-                                    "2. Si quieres crear un Warrior \n");
+                            System.out.println("Invalid number");
+                            System.out.println("Please select: \n" +
+                                    "1. If you want to create a wizard \n" +
+                                    "2. If you want to create a warrior \n");
                             option = scanner2.nextInt();
                         }switch(option){
                             case 1:
-                                System.out.println("Creando un mago...");
+                                System.out.println("Creating a wizard...");
                                 Character wizard = createCustomizedWizard();
                                 team1.addWizard((Wizard) wizard);
-                                System.out.println("Mago creado exitosamente!");
+                                System.out.println("Wizard successfully created!");
                                 break;
                             case 2:
-                                System.out.println("Creando un guerrero...");
+                                System.out.println("Creating a warrior...");
                                 Character warrior = createCustomizedWarrior();
                                 team1.addWarrior((Warrior) warrior);
-                                System.out.println("Guerrero creado exitosamente!");
+                                System.out.println("Warrior successfully created!");
                                 break;
                             }
 
-                        System.out.println("Numero de personajes creados: " +i+ "    Numero total del equipo: "+numMemberParty);
+                        System.out.println("Number of characters created: " +i+ "    Total team number: "+numMemberParty);
                         }
-                    System.out.println("La party del team "+nameTeam1+" se ha creado exitosamante");
+                    System.out.println("The "+nameTeam1+" team's party has been successfully created");
                     team1.printParty();
 
                     // Team 2 -------------------------------------------
-                    System.out.println("¿Cual quieres que sea el nombre del equipo 2?");
+                    System.out.println("What do you want your team 2 to be called?");
                     nameTeam2 = scanner.next();
-                    System.out.println("El nombre del Team 2 es: " + nameTeam2);
+                    System.out.println("The name of the team 2 is: " + nameTeam2);
                     team2 = new CustomizedParty(numMemberParty, nameTeam2);
 
                     for(int i = 1; i <= numMemberParty; i++){
                         Scanner scanner2 = new Scanner(System.in);
-                        System.out.println("Por favor seleccionar: \n" +
-                                "1. Si quieres crear un Wizard \n" +
-                                "2. Si quieres crear un Warrior \n");
+                        System.out.println("Please select: \n" +
+                                "1. If you want to create a wizard \n" +
+                                "2. If you want to create a warrior \n");
                         option = scanner2.nextInt();
                         while (option != 1 && option != 2){
-                            System.out.println("Numero invalido");
-                            System.out.println("Por favor seleccionar: \n" +
-                                    "1. Si quieres crear un Wizard \n" +
-                                    "2. Si quieres crear un Warrior \n");
+                            System.out.println("Invalid number");
+                            System.out.println("Please select: \n" +
+                                    "1. If you want to create a wizard \n" +
+                                    "2. If you want to create a warrior \n");
                             option = scanner2.nextInt();
                         }switch(option){
                             case 1:
-                                System.out.println("Creando un mago...");
+                                System.out.println("Creating a wizard...");
                                 Character wizard = createCustomizedWizard();
                                 team2.addWizard((Wizard) wizard);
-                                System.out.println("Mago creado exitosamente!");
+                                System.out.println("Wizard successfully created!");
                                 break;
                             case 2:
-                                System.out.println("Creando un guerrero...");
+                                System.out.println("Creating a warrior...");
                                 Character warrior = createCustomizedWarrior();
                                 team2.addWarrior((Warrior) warrior);
-                                System.out.println("Guerrero creado exitosamente!");
+                                System.out.println("Warrior successfully created!");
                                 break;
                         }
 
-                        System.out.println("Numero de personajes creados: " +i+ "    Numero total del equipo: "+numMemberParty);
+                        System.out.println("Number of characters created: " +i+ "   Total team number: "+numMemberParty);
                     }
-                    System.out.println("La party del team "+nameTeam2+" se ha creado exitosamante");
+                    System.out.println(("The "+nameTeam2+" team's party has been successfully created"));
                     team2.printParty();
 
 
@@ -191,15 +165,13 @@ public class Main {
                     team2 = new RandomParty(5, "Random Team 2");
             }
 
-        System.out.println("Que comience la batalla" );
+        System.out.println("Let the battle begin!" );
         while(team1.isTeamLive() && team2.isTeamLive()){
-            System.out.println("¿Qué jugador quieres que luche en primer lugar para el equipo 1? Por favor ingresa el id" );
+            System.out.println("Which player of team 1 do you want to fight first? Please insert his ID number" );
         }
         //for (int i = 1; i<= 2; i++){
         //    int idPersonaje = scanner.nextInt();
         //}
-
-
 
 
     }
